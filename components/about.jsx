321d9927bc0d358cb9a2edec0abf10772513b9c6
Dedicio Coelho <dedicio@gmail.com>
2022-01-20 21:01:31 -0300

    fix: improving translate

--- components/about.jsx
+++ components/about.jsx
@@ -10,19 +10,19 @@ const About = (props) => {
         ? <div>
             <p><strong>My goal is to do things that make people&apos;s lives easier!</strong></p>
 
-            <p>Born in a small country town, curious since I was a child, the knowledge is my passion! I graduated in Audiovisual Production, my career with internet has been started working as a web designer.</p>
+            <p>Born in a small country town, curious since I was a child, knowledge is my passion! I graduated in Audiovisual Production, and my career with the internet started when I began working as a web designer.</p>
 
-            <p>With my passion for technology and my eagerness to make things work, my migration to developing was natural.</p>
+            <p>My enthusiasm for technology and my eagerness to make things work made my migration to developing natural.</p>
 
-            <p>For over 10 years working with the web, my eyes continue to shine on the mission to develop products and services that people use and make a difference in their lives.</p>
+            <p>After 12 years working with the internet, my eyes still light up for  the mission to develop products and services that people use and which make a difference in their lives.</p>
           </div>
 
         : <div>
             <p><strong>Meu objetivo é fazer coisas que facilitem a vida das pessoas!</strong></p>
 
-            <p>Nascido em uma cidadezinha pequena do interior, curioso desde criança, o conhecimento é minha paixão! Sou formado em Produção Audiovisual, minha carreira com web se iniciou trabalhando como web designer.</p>
+            <p>Nascido em uma cidadezinha pequena do interior, curioso desde criança, conhecimento é minha paixão! Sou formado em Produção Audiovisual, minha carreira com web se iniciou trabalhando como web designer.</p>
 
-            <p>Com minha paixão por tecnologia e meu anseio por fazer as coisas funcionarem, foi natural a minha migração para o desenvolvimento.</p>
+            <p>Com minha atração por tecnologia e meu anseio por fazer as coisas funcionarem, foi natural a minha migração para o desenvolvimento.</p>
 
             <p>Há mais de 10 anos trabalhando com web, meus olhos continuam brilhando na missão de desenvolver produtos e serviços que as pessoas usem e façam diferença na vida delas.</p>
           </div>
